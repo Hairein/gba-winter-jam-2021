46a22098354b7cc223426a1496fd9c7bda5d5904
Micah Koleoso <maakoleoso@gmx.de>
2021-11-30 17:38:15 +0100

    Starting structure build-up for jam

--- gbawj2021_mksgame/src/main.cpp
+++ gbawj2021_mksgame/src/main.cpp
@@ -4,6 +4,7 @@
 #include "bn_optional.h"
 #include "bn_bg_palettes.h"
 #include "bn_sprite_text_generator.h"
+#include "bn_random.h"
 
 #include "common_info.h"
 #include "common_variable_8x16_sprite_font.h"
@@ -21,8 +22,42 @@ int main()
 
     common::info info("Hi there", info_text_lines, text_generator);
     
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
+    
+    int counter = 0;
+    int max_counter = 60;
+
+    bn::random random_generator;
+    int random_range = 101;
+
     while(true)
     {
+        if(counter == 0)
+        {
+            //int random_value = random_generator.get() % random_range;
+            unsigned int random_value = random_generator.get();
+
+            bn::string<32> text;
+            bn::ostringstream text_stream(text);
+            text_stream.append(random_value);
+
+            bn::string<32> text1;
+            bn::ostringstream text_stream1(text1);
+            text_stream1.append((random_value % random_range));
+            text_stream1.append("%");
+
+            text_sprites.clear();
+
+            text_generator.generate(0, 0, text, text_sprites);
+            text_generator.generate(0, 10, text1, text_sprites);
+        }
+        
+        counter++;
+        while(counter >= max_counter)
+        {
+            counter -= max_counter;
+        }
+
         info.update();
 
         bn::core::update();
---
+++ maps/world_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="world_tiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="world_tiles.bmp" trans="000000" width="128" height="128"/>
+</tileset>
